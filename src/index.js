73c93a035f34f1ae90513ee69405690e5b4a9da1
Valera Mamrovsky <valeramamrovsky@gmail.com>
2023-01-16 15:28:43 +0300

    feat: task answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj1 = {};
+  let obj2 = {};
+  bracketsConfig.forEach((item) => obj1[item[1]] = item[0]);
+  bracketsConfig.forEach((item) => obj2[item[0]] = item[1]);
+  let arr = [];
+  for(let i = 0; i < str.length; i++) {
+    if (Object.values(obj1).includes(str[i])) {
+      if (str[i] !== obj2[str[i]]) arr.push(str[i]);
+      else {
+        if (str[i] !== arr[arr.length - 1]) arr.push(str[i]);
+        else arr.splice(arr.length - 1, 1);
+      }
+    }
+    else if (arr[arr.length - 1] === obj1[str[i]]) arr.splice(arr.length - 1, 1);
+    else return false;
+  }
+  if (arr.length === 0) return true;
+  else return false;
 }
